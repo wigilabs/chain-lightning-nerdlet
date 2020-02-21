974753072e35f3ab249672a41c685db450648412
oil <le.rue@protonmail.com>
2020-02-21 17:10:26 -0500

    zabbix service

--- nerdlets/chain-lighting-app-nerdlet-nerdlet/index.js
+++ nerdlets/chain-lighting-app-nerdlet-nerdlet/index.js
@@ -103,7 +103,7 @@ export default class ChainLightingAppNerdlet extends React.Component {
         super(props);
         this.accountId = 2482859;
         this.state = {
-            value: "NagiosReport",
+            value: "SELECT * FROM NagiosReport SINCE 1 week ago LIMIT 1",
             showModal: false,
             nagiosP: 100,
             zabbixP: 100,
@@ -129,8 +129,14 @@ export default class ChainLightingAppNerdlet extends React.Component {
         let consistency = new Lightning(document.getElementById('js-consistency'), l, 3.5)
 
         let self = this
-
-        setTimeout(function () {
+        function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+                            }
+        setTimeout(async function () {
+         const nrql1="SELECT * FROM NagiosReport SINCE 1 week ago LIMIT 1"
+         const nrql2="SELECT description from ZabbixHTTP SINCE 1 week ago LIMIT 1"
+         const nrql3="SELECT description from ZabbixSSH SINCE 1 week ago LIMIT 1"
+         const nrql4="SELECT description from ZabbixAgent SINCE 1 week ago LIMIT 1"
             let container = document.querySelector('.AFAEAU--vz--chart-container')
             console.log('container: ', container)
             let content = JSON.parse(container.innerText)
@@ -160,11 +166,69 @@ export default class ChainLightingAppNerdlet extends React.Component {
 
             self.setState({nagiosP: p});
             console.log('self.state.nagiosP: ', self.state.nagiosP)
-
+            //ZabbixScore
+            let zabbixScore=0;
+            console.log("=> Initial Score ")
+            //ZabbixHTTP
+            self.setState({value :"SELECT description from ZabbixHTTP SINCE 1 week ago LIMIT 1" })
+            await sleep(4000);
+            console.log('query update: ', self.state.value)
+            let container1 = document.querySelector('.chart2')
+            console.log('container1: ', container1)
+            let content1 = JSON.parse(container1.innerText)
+            console.log('content1: ', content1)
+            let obj1=content1[0].data[0]
+            console.log(" > Status", obj1.description)
+            let eval1=obj1.description
+            if (eval1.includes("is unreachable")){
+                zabbixScore=zabbixScore
+                //o poner hacer nada, no se yo prefiero que haga algo :)
+            } else {
+                zabbixScore=zabbixScore+1
+                //suma un punto
+            } 
+            //ZabbixSSH
+            self.setState({value :"SELECT description from ZabbixSSH SINCE 1 week ago LIMIT 1" })
+            await sleep(4000);
+            console.log('query update: ', self.state.value)
+            let container2 = document.querySelector('.chart2')
+            console.log('container2: ', container2)
+            let content2 = JSON.parse(container2.innerText)
+            console.log('content2: ', content2)
+              let obj2=content2[0].data[0]
+            console.log(" > Status", obj2.description)
+            let eval2=obj2.description
+            if(eval2.includes("is unreachable")){
+                 zabbixScore=zabbixScore
+                //o poner hacer nada, no se yo prefiero que haga algo :)
+            } else {
+                 zabbixScore=zabbixScore+1
+                //suma un punto
+            }
+            //ZabbixAgent
+            self.setState({value :"SELECT description from ZabbixAgent SINCE 1 week ago LIMIT 1" })
+            await sleep(4000);
+            console.log('query update: ', self.state.value)
+            let container3 = document.querySelector('.chart2')
+            console.log('container3: ', container3)
+            let content3 = JSON.parse(container3.innerText)
+            console.log('content3: ', content3)
+            let obj3=content3[0].data[0]
+            console.log(" > Status", obj3.description)
+            let eval3=obj3.description
+            if(eval3.includes("is unreachable")){
+              zabbixScore=zabbixScore  
+            } else {
+              zabbixScore=zabbixScore+1  
+            }
+            let zabbixAudit=zabbixScore/3*100
+            console.log("Zabbix status=> ",zabbixAudit,"%")
+            self.setState({"zabbixP":zabbixAudit})
+            //Splunk
+            
         }, 1000)
       }
     render() {
-         const nrql="SELECT * FROM NagiosReport SINCE 1 week ago"
          const innerText = require('react-innertext');
          console.log("js: working!")
          console.log('JsonChart: ', JsonChart)
@@ -204,17 +268,15 @@ export default class ChainLightingAppNerdlet extends React.Component {
                     <article className="service bottom-left" style={{backgroundImage: 'linear-gradient(90deg, #14FFFF 100%, transparent 100%)'}}>
                       <img src="https://pbs.twimg.com/profile_images/463695202377420800/Puzeh-5R_400x400.jpeg" alt="description of image"/>
                     </article>
-
                     <p className="text">Function    <span>80%</span></p>
                     <canvas className="canvas" id="js-function"></canvas>
                     <p className="text">Speed       <span>100%</span></p>
                     <canvas className="canvas" id="js-speed"></canvas>
                     <p className="text">Consistency <span>100%</span></p>
                     <canvas className="canvas" id="js-consistency"></canvas>
-
                       <JsonChart
                        id="dataTableNagios"
-                       query={"SELECT * FROM " + this.state.value + " SINCE 1 week ago LIMIT 1"}
+                       query={this.state.value}
                        accountId={this.accountId}
                        className="chart2"
                       />
